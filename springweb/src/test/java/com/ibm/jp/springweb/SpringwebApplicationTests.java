3becb77be63a9d9d0260637ec83c0a0f4def5a22
takanashimikuni <ruby_fifteen324@yahoo.co.jp>
2020-02-12 15:01:59 +0900

    first commit

---
+++ springweb/src/test/java/com/ibm/jp/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.jp.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
